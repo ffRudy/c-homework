e028520d9b3a53d3afd1c12d81f118df20e2e4d5
ffRudy <1820249231@qq.com>
2020-02-23 21:45:16 +0800

    create repository

---
+++ project1/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        int n1, n2,res;
+        string myOperator;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            n1 = int.Parse(num1.Text);
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            n2 = int.Parse(num2.Text);
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            myOperator = "add";//保存运算符 加号
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            myOperator = "sub";//保存运算符 减号
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            myOperator = "mul";//保存运算符 乘号
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            myOperator = "div";//保存运算符 除号
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            switch (myOperator)//用switch进行响应的分支操作
+            {
+                case "add"://加号
+                    res = n1 + n2;
+                    result.Text = res.ToString();
+                    break;
+                case "sub"://减号
+                    res = n1 - n2;
+                    result.Text = res.ToString();
+                    break;
+                case "mul"://乘号
+                    res = n1 * n2;
+                    result.Text = res.ToString();
+                    break;
+                case "div"://除号
+                    if (n2 == 0)//除数为0报错
+                    {
+                        result.Text = "除数不能为“0”！";
+                    }
+                    else
+                    {
+                        res = n1 / n2;
+                        result.Text = res.ToString();
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ project2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入第一个数字：");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("请输入第二个数字？");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("请输入运算符？");
+            char myOperator = char.Parse(Console.ReadLine());
+            int result;
+            switch (myOperator)//用switch进行响应的分支操作
+            {
+                case '+'://加号
+                    result = num1 + num2;
+                    Console.WriteLine("运算结果为："+result);
+                    break;
+                case '-'://减号
+                    result = num1 - num2;
+                    Console.WriteLine("运算结果为：" + result);
+                    break;
+                case '*'://乘号
+                    result = num1 * num2;
+                    Console.WriteLine("运算结果为：" + result);
+                    break;
+                case '/'://除号
+                    if (num2 == 0)//除数为0报错
+                    {
+                        Console.WriteLine("除数不能为“0”！");
+                    }
+                    else
+                    {
+                        result = num1 / num2;
+                        Console.WriteLine(result);
+                    }
+                    break;
+            }
+        }
+    }
+}
