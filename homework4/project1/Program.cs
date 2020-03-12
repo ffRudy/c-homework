a0356ae01df8174093259db10458c12e2a51a7b2
ffRudy <1820249231@qq.com>
2020-03-12 23:20:05 +0800

    create repository

---
+++ homework4/project1/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace home4_1
+{ // 链表节点
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    //泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            if (action == null)
+            {
+                throw new ArgumentNullException(nameof(action));
+            }
+
+            Node<T> head = Head;
+            while (head != null)
+            {
+                action(head.Data);
+                head = head.Next;
+                if (head == null)
+                {
+                    break;
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 整型List
+            GenericList<int> intlist = new GenericList<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                intlist.Add(x);
+            }
+            for (Node<int> node = intlist.Head;
+                  node != null; node = node.Next)
+            {
+                Console.WriteLine(node.Data);
+            }
+
+            // 字符串型List
+            GenericList<string> strList = new GenericList<string>();
+            for (int x = 0; x < 10; x++)
+            {
+                strList.Add("str" + x);
+            }
+            for (Node<string> node = strList.Head;
+                    node != null; node = node.Next)
+            {
+                Console.WriteLine(node.Data);
+            }
+            int max = -1000000;
+            int min = 1000000;
+            int sum = 0;
+            intlist.ForEach((x) =>
+            {
+                if (x > max) { max = x; }
+                if (x < min) { min = x; }
+                sum += x;
+                Console.WriteLine("目前最大值为：" + max);
+                Console.WriteLine("目前最小值为：" + min);
+                Console.WriteLine("目前和为：" + sum);
+            });
+
+        }
+
+    }
+}
---
+++ homework4/project2/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace home4_2
+{
+    public delegate void AlarmHandler(int h,int m,int s);
+    public delegate void TickHandler(int h,int m,int s);
+    public class Clock
+    {
+        public event AlarmHandler Alarm;
+        public event TickHandler Tick;
+        private int hour;
+        private int minute;
+        private int second;
+        private int alarm_hour;
+        private int alarm_minute;
+        private int alarm_second;
+        public void Start()
+        {
+            this.hour = DateTime.Now.Hour;
+            this.minute = DateTime.Now.Minute;
+            this.second = DateTime.Now.Second;
+        }
+        public void ClockTick()
+        {
+            this.hour = DateTime.Now.Hour;
+            this.minute = DateTime.Now.Minute;
+            this.second = DateTime.Now.Second;
+            if (this.hour == this.alarm_hour && this.minute == this.alarm_minute && this.second == this.alarm_second)
+            {
+                return;
+            }
+            else
+            {
+                Tick(this.hour, this.minute, this.second);
+            }
+        }
+        public void ClockAlarm()
+        {
+            if (this.hour == this.alarm_hour && this.minute == this.alarm_minute && this.second == this.alarm_second){ Alarm(this.hour, this.minute, this.second); }
+        }
+        public void SetAlarm(int h,int m,int s)
+        {
+            this.alarm_hour = h;
+            this.alarm_minute = m;
+            this.alarm_second = s;
+        }
+    }
+    public class Form
+    {
+        public Clock clock = new Clock();
+        public Form()
+        {
+            clock.Alarm += new AlarmHandler(Clock_Alarm);
+            clock.Tick += new TickHandler(Clock_Tick);
+        }
+        void Clock_Alarm(int h,int m,int s)
+        {
+            Console.WriteLine("闹钟时间到了！！！");
+        }
+        void Clock_Tick(int h,int m,int s)
+        {
+            Console.WriteLine(h + "时 " + m + "分 " + s + "秒");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form form1 = new Form();
+            form1.clock.Start();
+            form1.clock.SetAlarm(23, 17, 10);
+            for(int i=0; ; i++)
+            {
+                form1.clock.ClockTick();
+                Thread.Sleep(1000);
+                form1.clock.ClockAlarm();
+            }
+        }
+    }
+}
